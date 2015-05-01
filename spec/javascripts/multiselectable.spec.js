23621ff6784681be3dfddaef8ae500d1b5f18eab
Ravi Kumar Hamsa <ravi.hamsa@inmobi.com>
2015-05-01 09:49:30 +0530

    jshint fixes

--- spec/javascripts/multiselectable.spec.js
+++ spec/javascripts/multiselectable.spec.js
@@ -1,77 +1,84 @@
-describe("MultiSelectable", function () {
-    var selectableCollection, selectionManager
+describe('MultiSelectable', function() {
+    var selectableCollection, selectionManager;
 
-    beforeEach(function () {
+    beforeEach(function() {
         selectionManager = new Backbone.MultiSelectManager();
-        selectableCollection = new Backbone.MultiSelectableCollection([{id: '1', name: 'one'}, {id: '2', name: 'two'}, {name:'noid'}]);
-        selectableCollection.on('all', function(arg1, arg2, arg3){
+        selectableCollection = new Backbone.MultiSelectableCollection([{id: '1', name: 'one'}, {id: '2', name: 'two'}, {name: 'noid'}]);
+        selectableCollection.on('all', function(arg1, arg2, arg3) {
             //console.log(arg1, arg2, arg3);
-        })
-    })
+        });
+    });
 
-    afterEach(function () {
+    afterEach(function() {
         selectableCollection = null;
     });
 
 
-    it('has selection manager', function () {
+    it('has selection manager', function() {
         expect(selectableCollection.selectionManager).toBeDefined();
-    })
+    });
 
-    it('selection manager validation pass', function () {
+    it('selection manager validation pass', function() {
         expect(selectionManager.validateSelection('test')).toBeDefined();
-    })
+    });
 
-    it('selection manager validation fail', function () {
+    it('selection manager validation fail', function() {
         expect(selectionManager.validateSelection()).toBeFalsy();
-    })
+    });
 
-    it('selection manager returns expected', function () {
+    it('selection manager returns expected', function() {
         selectionManager.setSelected('ravi');
         expect(selectionManager.getSelected()['ravi']).toBeTruthy();
-    })
+    });
 
-    it('selectable validation fails on missing model', function(){
+    it('selectable validation fails on missing model', function() {
         expect(selectableCollection.validateSelection()).toBeFalsy();
-    })
+    });
 
-    it('selectable validation fails on id missing', function(){
+    it('selectable validation fails on id missing', function() {
         expect(selectableCollection.validateSelection(selectableCollection.at(2))).toBeFalsy();
-    })
+    });
 
-    it('selectable validation fails on external model', function(){
-        expect(selectableCollection.validateSelection(new Backbone.Model({id:1, name:'ravi'}))).toBeFalsy();
-    })
+    it('selectable validation fails on external model', function() {
+        expect(selectableCollection.validateSelection(new Backbone.Model({id: 1, name: 'ravi'}))).toBeFalsy();
+    });
 
-    it('selectable validation pass', function(){
+    it('selectable validation pass', function() {
         expect(selectableCollection.validateSelection(selectableCollection.at(0))).toBeTruthy();
-    })
+    });
 
-    it('selectable selection works', function(){
+    it('selectable selection works', function() {
         selectableCollection.setSelected(selectableCollection.at(0));
         var selected = selectableCollection.getSelected();
         expect(selected).toBeDefined();
-    })
+    });
 
-    it('selectable selection fail in missing model ', function(){
+    it('selectable selection fail in missing model ', function() {
         selectableCollection.setSelected();
         var selected = selectableCollection.getSelected();
         expect(selected.length).toBe(0);
-    })
+    });
 
-    it('selectable selection works as expected ', function(){
+    it('selectable selection works as expected ', function() {
         selectableCollection.setSelected(selectableCollection.get(1));
         var selected = selectableCollection.getSelected();
         expect(selected[0]).toBe(selectableCollection.get(1));
-    })
+    });
 
-    it('selectable selection works as expected on second select ', function(){
+    it('selectable selection works as expected on second select ', function() {
         selectableCollection.setSelected(selectableCollection.get(1));
         selectableCollection.setSelected(selectableCollection.get(2));
         var selected = selectableCollection.getSelected();
         expect(selected.length).toBe(2);
-    })
+    });
+
+    it('selectable selection works as expected on reselect ', function() {
+        selectableCollection.setSelected(selectableCollection.get(1));
+        selectableCollection.setSelected(selectableCollection.get(2));
+        selectableCollection.setSelected(selectableCollection.get(1));
+        var selected = selectableCollection.getSelected();
+        expect(selected.length).toBe(1);
+    });
 
 
-
-})
+});
--- spec/javascripts/setup.spec.js
+++ spec/javascripts/setup.spec.js
@@ -1,17 +1,17 @@
-describe("Setup Alright", function(){
+describe('Setup Alright', function() {
 
-    it('Backbone Loaded', function(){
-        expect(Backbone).toBeDefined()
-    })
+    it('Backbone Loaded', function() {
+        expect(Backbone).toBeDefined();
+    });
 
-    it('Backbone.Selectable Loaded', function(){
-        expect(Backbone.SelectableCollection).toBeDefined()
-    })
+    it('Backbone.Selectable Loaded', function() {
+        expect(Backbone.SelectableCollection).toBeDefined();
+    });
 
-    it('Backbone.MultiSelectable Loaded', function(){
-        expect(Backbone.MultiSelectableCollection).toBeDefined()
-    })
+    it('Backbone.MultiSelectable Loaded', function() {
+        expect(Backbone.MultiSelectableCollection).toBeDefined();
+    });
 
 
 
-})
+});
